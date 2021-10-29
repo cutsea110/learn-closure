8b24d57355a98c19229a53d6d511a349d118b80b
cutsea110 <cutsea110@gmail.com>
2021-10-30 06:57:26 +0900

    example 1,2,3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,57 @@
 fn main() {
     println!("Hello, world!");
     call_with_42(|x| 2*x);
+
+    // example 1
+    // ok
+    let x = Data(0);
+    call_fn_once(|| {
+      consume(x);
+    });
+    // error : cannot move ut of `x`, a captured variable in an `FnMut` closure
+    // let x = Data(0);
+    // call_fn_mut(|| {
+    //   consume(x)
+    // });
+    // error : cannot move ut of `x`, a captured variable in an `Fn` closure
+    // let x = Data(0);
+    // call_fn(|| {
+    //   consume(x);
+    // });
+
+    // example 2
+    // ok
+    let mut x = Data(0);
+    call_fn_once(|| {
+      x.0 = 1;
+    });
+    // ok
+    let mut x = Data(0);
+    call_fn_mut(|| {
+      x.0 = 1;
+    });
+    // error : cannot assign to `x.0` as `Fn` closures cannot mutate their captured variables
+    // let mut x = Data(0);
+    // call_fn(|| {
+    //   x.0 = 1;
+    // });
+
+    // example 3
+    // ok
+    let x = Data(0);
+    call_fn_once(|| {
+      println!("{}", x.0);
+    });
+    // ok
+    let x = Data(0);
+    call_fn_mut(|| {
+      println!("{}", x.0);
+    });
+    // ok
+    let x = Data(0);
+    call_fn(|| {
+      println!("{}", x.0);
+    });
 }
 
 // an example of closure
@@ -25,3 +76,21 @@ pub trait Fn<Args> : FnMut<Args> {
   extern "rust-call" fn call(&self, args: Args) -> Self::Output;
 }
 */
+
+fn call_fn_once<F>(f: F) where F: FnOnce() {
+   f();
+}
+
+fn call_fn_mut<F>(mut f: F) where F: FnMut() {
+   f();
+}
+
+fn call_fn<F>(f: F) where F: Fn() {
+   f();
+}
+
+struct Data(i32);
+
+fn consume(x: Data) {
+   println!("{}", x.0);
+}
